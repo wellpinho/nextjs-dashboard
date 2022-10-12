f930d3fd8c9bb005cee56c1f20603d288bac48e7
wellpinho <wellpinho@outlook.com>
2022-10-12 10:10:57 -0300

    ✨ NEW FEATURE: input component

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+const Input = ({ name, label, ...rest }: InputProps) => {
+  return (
+    <FormControl>
+      {/* só exibe se a label existir */}
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+};
+
+export default Input;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
+import { Button, Flex, FormControl, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Input from "../components/form/Input";
 
-const Home: NextPage = () => {
+const Signin: NextPage = () => {
   return (
-    <div>
-      <h1>DashGo</h1>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth="360"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+        flexDirection="column"
+      >
+        {/*  stack é para dar espaçamentos entre os itens */}
+        <Stack spacing="2">
+          <Input name="email" type="email" label="Email" />
+
+          <FormControl>
+            <Input name="password" type="password" label="Password" />
+          </FormControl>
+
+          <Button type="submit" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   );
 };
 
-export default Home;
+export default Signin;
